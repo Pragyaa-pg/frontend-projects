7ea44e786af4ef8d3b2edac38182790af876f0e2
Pragyaa-pg <aaryapg17@gmail.com>
2026-01-12 23:40:36 +0530

    add css and js file

---
+++ landing page/script.js
@@ -0,0 +1,14 @@
+{/* <script> */}
+  const elements = document.querySelectorAll('.content');
+
+  window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+      const position = el.getBoundingClientRect().top;
+      const screenHeight = window.innerHeight;
+
+      if (position < screenHeight - 100) {
+        el.classList.add('show');
+      }
+    });
+  });
+{/* </script> */}
